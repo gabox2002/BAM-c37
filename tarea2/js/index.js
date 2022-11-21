c0adea3dff54c155554eca69e914db96851e4f21
gabox2002 <gabox2002@gmail.com>
2022-11-20 21:00:11 -0300

    m

---
+++ tarea2/js/index.js
@@ -0,0 +1,21 @@
+
+console.log ("Buenos días, soy el código del archivo externo del html")
+
+console.log ('Buenos días, soy el código del archivo externo del html');
+
+//Variebles 
+//Palabras reservadas
+var nombre;
+
+nombre = "Gabriel";
+
+console.log ("Mi nombre es: " + nombre);
+
+let apellido = "Quispe";
+
+const provincia = "Salta";
+
+nombre = "Pepe";
+apellido = "Sanchez";
+
+console.log ("Mi nombre es $(nombre) y mi apellido es $(apellido) ");
